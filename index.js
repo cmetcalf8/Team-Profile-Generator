6851d50f5da9cec55c8e31235d4c4c45e919eb7e
Cory Metcalf <coco@Corys-MacBook-Pro.local>
2022-08-21 14:13:00 -0600

    added engineer and intern questions

--- index.js
+++ index.js
@@ -35,3 +35,73 @@ function addManager() {
     });
   
   }
+
+  function addEngineer() {
+    inquirer.prompt ([
+      
+      {
+        type: "input",
+        name: "engineerName",
+        message: "What is the engineer's name?"
+      },
+  
+      {
+        type: "input",
+        name: "engineerId",
+        message: "What is the engineer's employee ID number?"
+      },
+  
+      {
+        type: "input",
+        name: "engineerEmail",
+        message: "What is the engineer's email address?"
+      },
+  
+      {
+        type: "input",
+        name: "engineerOfficeNumber",
+        message: "What is the engineer's office number?"
+      }
+  
+    ]).then(answers => {
+      const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerOfficeNumber);
+      teamArray.push(engineer);
+      createTeam();
+    });
+  
+  }
+
+  function addIntern() {
+    inquirer.prompt ([
+      
+      {
+        type: "input",
+        name: "internName",
+        message: "What is the intern's name?"
+      },
+  
+      {
+        type: "input",
+        name: "internId",
+        message: "What is the intern's employee ID number?"
+      },
+  
+      {
+        type: "input",
+        name: "internEmail",
+        message: "What is the intern's email address?"
+      },
+  
+      {
+        type: "input",
+        name: "internOfficeNumber",
+        message: "What is the intern's office number?"
+      }
+  
+    ]).then(answers => {
+      const intern = new intern(answers.internName, answers.internId, answers.internEmail, answers.internOfficeNumber);
+      teamArray.push(intern);
+      createTeam();
+    });
+  
+  }
