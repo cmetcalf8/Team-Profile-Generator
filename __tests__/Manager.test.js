862aacfe0c5228b8f156c613d6b70cb6409a5f85
Cory Metcalf <coco@Corys-MacBook-Pro.local>
2022-08-21 14:28:47 -0600

    finished manager test

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,7 +1,22 @@
+const { default: test } = require("node:test");
+const { hasUncaughtExceptionCaptureCallback } = require("process");
+
 const Manager = requirie("../lib/Manager");
 
 test("Can create an office number.", () => {
-    const testOfficeNumber = 2;
-    const employeeInstance = new Manager ("Cory", 2, "cmetcalf8@gmail.com", testOfficeNumber);
+    const testOfficeNumber = 1;
+    const employeeInstance = new Manager ("Cory", 1, "cmetcalf8@gmail.com", testOfficeNumber);
     hasUncaughtExceptionCaptureCallback(employeeInstance.testOfficeNumber).toBe(testOfficeNumber);
+});
+
+test("Will officNumber return office number", () => {
+    const testOfficeNumber = 1;
+    const employeeInstance = new Manager("Cory", 1, "cmetcalf8@gmail.com", testOfficeNumber);
+    hasUncaughtExceptionCaptureCallback(employeeInstance.getOfficeNumber()).toBe(testOfficeNumber);
+});
+
+test("Role test", () => {
+    const returnValue = "Manager";
+    const employeeInstance = new Manager("Cory", 1, "cmetcalf8@gmail.com", 1);
+    expect(employeeInstance.getRole()).toBe(returnValue);
 });
